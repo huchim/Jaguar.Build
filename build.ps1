bfbe967078f97d63728077e45329ec25bfc0cb4e
huchim <huchim@live.com.mx>
2016-10-04 16:51:21 -0500

    Cambios menores.

--- build.ps1
+++ build.ps1
@@ -1,43 +1,29 @@
-﻿Write-Host -ForegroundColor Green "Iniciando compilación..."
+﻿# Nombre del archivo de la solución recibido desde quien ejecuta esta rutina.
+$SolutionName = $env:SOLUTION_NAME
 
 # Directorio de la solución.
-# $RepositoryRoot = Convert-Path (Get-Location)
-$RepositoryRoot = "F:\users\huchim\documents\visual studio 2015\Projects\github\"
-$SolutionName = "Jaguar.Core.DAL"
-$SolutionPath = Join-Path $RepositoryRoot "$SolutionName\$SolutionName.sln"
-# Directorio donde se compilará.
-$BuildRoot = Join-Path $RepositoryRoot "$SolutionName\src\$SolutionName\bin\Release"
+$RepositoryRoot = Convert-Path (Get-Location)
+$BuildRoot = Join-Path $RepositoryRoot "\src\$SolutionName\bin\Release"
+$SolutionPath = Join-Path $RepositoryRoot "$SolutionName.sln"
+
+# Herramientas
 $BuildLog =  Join-Path $BuildRoot "Build.log"
 $MSBuildDir = Join-Path $env:systemroot  "Microsoft.NET\Framework\v4.0.30319"
-$MsBuildExe = Join-Path $MSBuildDir "MsBuild.exe"  
 
-# Determinar a dónde va esta variable.
-$BuildRoot2 = (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Definition)))
-#$KoreBuildRoot = (Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)))
 
-Write-Host -ForegroundColor Green "RepositoryRoot: $BuildRoot"
+Write-Host -ForegroundColor Green "Creando la carpeta de destino..."
 
 if(Test-Path $BuildRoot) {
     del $BuildRoot -Force -Recurse
 }
 
+mkdir $BuildRoot | Out-Null
+
+Write-Host -ForegroundColor Green "Iniciando compilación..."
 $MSBuildResponseFile = Join-Path $BuildRoot "build.msbuild.rsp"
 if(Test-Path $MSBuildResponseFile) {
     del $MSBuildResponseFile
 }
-
-function exec($cmd) {
-    $cmdName = [IO.Path]::GetFileName($cmd)
-    Write-Host -ForegroundColor DarkGray "> $cmdName $args"
-    "`r`n>>>>> $cmd $args <<<<<`r`n" >> $KoreBuildLog
-    & $cmd @args 2>&1 >> $KoreBuildLog
-    $exitCode = $LASTEXITCODE
-    if($exitCode -ne 0) {
-        throw "'$cmdName $args' failed with exit code: $exitCode. See '$ArtifactsDir\korebuild.log' for details"
-    }
-}
-
-mkdir $BuildRoot | Out-Null
 
 $MSBuildArguments = @"
 /nologo
@@ -50,7 +36,7 @@ $MSBuildArguments = @"
 $MSBuildArguments | Out-File -Encoding ASCII -FilePath $MSBuildResponseFile
 $args | ForEach { $_ | Out-File -Append -Encoding ASCII -FilePath $MSBuildResponseFile }
 
-Write-Host -ForegroundColor DarkGray "> msbuild $SolutionName.sln $args"
+Write-Host -ForegroundColor DarkGray "> msbuild $SolutionName.sln "
 
 & "$MSBuildDir\MSBuild.exe" `@"$MSBuildResponseFile"
 
