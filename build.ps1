72a288d578ff66246a8887ce987bd296ddc9fe01
huchim <huchim@live.com.mx>
2016-10-04 17:52:31 -0500

    Crear el archivo de definición para paquetes NUGET
    Crear la definición del paquete únicamente si no existe.

--- build.ps1
+++ build.ps1
@@ -3,31 +3,33 @@ $SolutionName = $env:SOLUTION_NAME
 
 # Directorio de la solución.
 $RepositoryRoot = Convert-Path (Get-Location)
-$BuildRoot = Join-Path $RepositoryRoot "\src\$SolutionName\bin\Release"
+$BuildRoot = Join-Path $RepositoryRoot ".build"
+$OutPutDir = Join-Path $RepositoryRoot "\src\$SolutionName\bin\Release"
 $SolutionPath = Join-Path $RepositoryRoot "$SolutionName.sln"
+$MainProject = Join-Path $RepositoryRoot "\src\$SolutionName\$SolutionName.csproj"
 
 # Herramientas
-$BuildLog =  Join-Path $BuildRoot "Build.log"
+$BuildLog =  Join-Path $OutPutDir "Build.log"
 $MSBuildDir = Join-Path $env:systemroot  "Microsoft.NET\Framework\v4.0.30319"
 
 
 Write-Host -ForegroundColor Green "Creando la carpeta de destino..."
 
-if(Test-Path $BuildRoot) {
-    del $BuildRoot -Force -Recurse
+if(Test-Path $OutPutDir) {
+    del $OutPutDir -Force -Recurse
 }
 
-mkdir $BuildRoot | Out-Null
+mkdir $OutPutDir | Out-Null
 
 Write-Host -ForegroundColor Green "Iniciando compilación..."
-$MSBuildResponseFile = Join-Path $BuildRoot "build.msbuild.rsp"
+$MSBuildResponseFile = Join-Path $OutPutDir "build.msbuild.rsp"
 if(Test-Path $MSBuildResponseFile) {
     del $MSBuildResponseFile
 }
 
 $MSBuildArguments = @"
 /nologo
-/p:OutputPath="$BuildRoot"
+/p:OutputPath="$OutPutDir"
 /fl
 /flp:LogFile="$BuildLog";Verbosity=diagnostic;Encoding=UTF-8
 "$SolutionPath"
@@ -38,6 +40,13 @@ $args | ForEach { $_ | Out-File -Append -Encoding ASCII -FilePath $MSBuildRespon
 
 Write-Host -ForegroundColor DarkGray "> msbuild $SolutionName.sln "
 
-& "$MSBuildDir\MSBuild.exe" `@"$MSBuildResponseFile"
+&"$MSBuildDir\MSBuild.exe" `@"$MSBuildResponseFile"
+
 
 del $MSBuildResponseFile
+
+if(!(Test-Path "$MainProject.nuspec")) {
+    Write-Host "$BuildRoot\NuGet.exe"
+	Write-Host -ForegroundColor DarkGray "> Creando paquete "
+	&"$BuildRoot\NuGet.exe" spec $MainProject
+}
